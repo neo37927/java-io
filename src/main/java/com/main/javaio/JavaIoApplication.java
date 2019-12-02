a9e47a1007a53079901d2ee0511a1574bfcd91bc
xiaolin <xiaolin@zhuanzhuan.com>
2019-12-02 17:10:48 +0800

    spring boot initial

---
+++ src/main/java/com/main/javaio/JavaIoApplication.java
@@ -0,0 +1,13 @@
+package com.main.javaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIoApplication.class, args);
+	}
+
+}
